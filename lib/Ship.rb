0fa4f4bb8ac1fc0508f39e668bd376eef74f5f3d
Logan Matheny <loganpaulmatheny@gmail.com>
2024-03-14 18:38:55 -0400

    Add sunk method to ship

--- lib/Ship.rb
+++ lib/Ship.rb
@@ -1,12 +1,20 @@
 
 class Ship 
-  attr_reader  :name, :health
+  attr_reader  :name, :length, :health
   
   def initialize(name, length)
     @name = name
     @length = length
     @health = length
+    @sunk = false
+  end 
 
+  def hit 
+    @heatlh - 1
+  end 
+
+  def sunk? 
+    @sunk
   end 
 
 end 
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe 'Ship' do
     it 'should be able to build ships' do
       ship = Ship.new('Logan\'s Frigate', 2)
       expect(ship.name).to eq('Logan\'s Frigate' )
+      expect(ship.length).to eq(2)
+      expect(ship.health).to eq(2)
+      expect(ship.sunk?).to eq(false)
     end 
   end 
 end 
