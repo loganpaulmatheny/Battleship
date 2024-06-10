b70f3a9b13813a55d7a78ab82c79d06b0fbda88f
Logan Matheny <loganpaulmatheny@gmail.com>
2024-06-10 08:24:00 -0400

    Save recent progress

--- lib/board.rb
+++ lib/board.rb
@@ -25,6 +25,65 @@ class Board
     @cells
   end 
 
+  def valid_placement?(ship, coordinates)
+    valid_length = check_ship_coordinates?(ship, coordinates)
+    consecutive_placement = consecutive_coordinates?(coordinates)
+
+    if valid_length == false || consecutive_placement == false 
+      return false
+    else 
+      return true
+    end 
+    
+  end 
+  
+  def check_ship_coordinates?(ship, coordinates)
+    if ship.length == coordinates.length
+      return true
+    else 
+      return false
+    end  
+  end 
+
+  def consecutive_coordinates?(coordinates)
+    letters = coordinates.map { |coord| coord[0] }
+    numbers = coordinates.map { |coord| coord[1..-1].to_i }
+  
+    all_letters_same = letters.uniq.size == 1
+    # Are all the letters the same? - basically this says count the unique characters
+    all_numbers_same = numbers.uniq.size == 1
+
+    # I could have read further along and that would have helped me solve this on my own 
+    # Helpful Enumerables
+    # Ruby has an enumerable method called each_cons that allows you to access consecutive elements in a collection. See the Ruby Docs for more details.
+    # Also consider if the enumerables any?, all?, none? would be helpful.
+  
+    letters_consecutive = true
+    # this might be called a flag
+    (1...letters.size).each do |i|
+    # the `...` is called an exclusive range vs. an inclusive one denoted by `..`
+    # why might you ask do we need to use exclusive rather than inclusive? 
+      # that is because we are starting at the second index rather than the first. so we want to ensure we go through letters.size - 1 rather than the entire array
+      if letters[i].ord != letters[i - 1].ord + 1
+      # .ord converts the value to the ASCII value thus you can compare their ASCII numbers
+        letters_consecutive = false
+        break
+      end
+    end
+  
+    numbers_consecutive = true
+    (1...numbers.size).each do |i|
+      if numbers[i] != numbers[i - 1] + 1
+        numbers_consecutive = false
+        break
+      end
+    end
+  
+    (all_letters_same && numbers_consecutive) || (all_numbers_same && letters_consecutive)
+    # This simply checks to make sure you either have consecutive letters or numbers and not diagonals 
+  end
+
+
 end 
 
 # require 'pry'; binding.pry
