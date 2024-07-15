96ff34c2c114c3bc2f698488aac16c4280e9d310
Logan Matheny <loganpaulmatheny@gmail.com>
2024-07-14 20:47:37 -0400

    Create logic to detect when ships are sunk

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,5 @@
 require_relative 'commands'
+require_relative 'player'
 
 class Turn 
   def initialize(player_board, computer_board, player, computer)
@@ -20,10 +21,13 @@ class Turn
     player_input = Commands.choose_coordinates    
    # Need to also make sure that the coordinate is a valid coordinate 
     if  Commands.check_guess(@player, player_input) && Commands.validate_coordinate(@computer_board, player_input) 
-     player_guess = @computer_board.cells[player_input]
-     player_guess.fire_upon
-     @player.guesses.push(player_input)
-     return player_guess
+      player_guess = @computer_board.cells[player_input]
+      player_guess.fire_upon
+      @player.guesses.push(player_input)
+      if player_guess.ship && player_guess.ship.sunk?
+        @computer.ship_sunk
+      end
+      return player_guess
     else 
       player_turn 
     end
@@ -35,13 +39,16 @@ class Turn
       computer_guess = @player_board.cells[random_cell]
       computer_guess.fire_upon
       @computer.guesses.push(random_cell)
+      if computer_guess.ship && computer_guess.ship.sunk?
+        @player.ship_sunk
+      end
       return computer_guess
     else 
       computer_turn
     end    
   end 
 
-  def render_guess_status(guess, cell = false)
+  def render_guess_status(guess, cell)
     if guess == "M"
       return "is a miss"
     elsif guess == "H"
@@ -52,22 +59,35 @@ class Turn
    end 
  
   def render_sunk_ship(cell)
-    if cell != false 
-      return cell.ship
+    if cell.ship.sunk? 
+      return cell.ship.name
     else 
       return null
     end  
   end 
   
   def display_results(player_guess, computer_guess)
-    puts "The players guess of #{player_guess.cell }  #{render_guess_status(player_guess.render, player_guess.cell)}"
-    puts "The computer' guess of #{computer_guess.cell} #{render_guess_status(computer_guess.render, computer_guess.cell)}" 
+    puts "The players guess of #{player_guess.cell }  #{render_guess_status(player_guess.render, player_guess)}"
+    puts "The computer' guess of #{computer_guess.cell} #{render_guess_status(computer_guess.render, computer_guess)}" 
   end
 
   def execute 
     display_boards
-    player_move = player_turn
-    computer_move = computer_turn
-    display_results(player_move, computer_move)
+    until game_over?
+      player_move = player_turn
+      computer_move = computer_turn
+      display_results(player_move, computer_move)
+    end
+    puts "Game over"
+    if @player.ships_remaining > 0
+      puts "Player wins!!"
+    else 
+      puts "Computer wins!"
+    end
   end
+
+  def game_over?
+    @player.ships_remaining == 0  || @computer.ships_remaining == 0 
+  end 
+
 end 
